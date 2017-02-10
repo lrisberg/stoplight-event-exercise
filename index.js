9e2f8d713616a1bce38f8b054e5900373b3a4c04
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-02-09 20:59:42 -0700

    Add part 1 finished

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 (function() {
-  'use strict';
+  document.addEventListener("DOMContentLoaded", function() {
+    'use strict';
 
-  // YOUR CODE HERE
+    // YOUR CODE HERE
+    let controls = document.getElementById('controls');
+    let buttons = document.getElementsByClassName('button');
+    let stopButton = document.getElementById('stopButton');
+    let slowButton = document.getElementById('slowButton');
+    let goButton = document.getElementById('goButton');
+
+    let stopLight = document.getElementById('stopLight');
+    let slowLight = document.getElementById('slowLight');
+    let goLight = document.getElementById('goLight');
+
+
+    controls.addEventListener('click', function(event) {
+      if (event.target === stopButton) {
+        console.log('You clicked the stop button');
+        stopLight.classList.toggle('stop');
+      } else if (event.target === slowButton) {
+        console.log('You clicked the slow button');
+        slowLight.classList.toggle('slow');
+      } else if (event.target === goButton) {
+        console.log('You clicked the go button!');
+        goLight.classList.toggle('go');
+      }
+    });
+
+
+  });
 })();
