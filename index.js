1987b20eedcabb20ffe6fa7eaafaeca2802bd22a
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-02-11 08:51:11 -0700

    complete part1 and 2; stoplights turn on and off independently of each other and mouseovers are logged

--- index.js
+++ index.js
@@ -13,19 +13,44 @@
     let slowLight = document.getElementById('slowLight');
     let goLight = document.getElementById('goLight');
 
+    let stopText = stopButton.innerText;
+    let slowText = slowButton.innerText;
+    let goText = goButton.innerText;
+
 
     controls.addEventListener('click', function(event) {
       if (event.target === stopButton) {
-        console.log('You clicked the stop button');
+        console.log(stopText + ' bulb on');
         stopLight.classList.toggle('stop');
       } else if (event.target === slowButton) {
-        console.log('You clicked the slow button');
+        console.log(slowText + ' bulb on');
         slowLight.classList.toggle('slow');
       } else if (event.target === goButton) {
-        console.log('You clicked the go button!');
+        console.log(goText + ' bulb on');
         goLight.classList.toggle('go');
       }
-    });
+    })
+
+    controls.addEventListener('mouseover', function(event) {
+      if (event.target === stopButton) {
+        console.log("Entered " + stopText + " button");
+      } else if (event.target === slowButton) {
+        console.log("Entered " + slowText + " button");
+      } else if (event.target === goButton) {
+        console.log("Entered " + goText + " button");
+      }
+    })
+
+    controls.addEventListener('mouseout', function(event) {
+      if (event.target === stopButton) {
+        console.log("Left " + stopText + " button");
+      } else if (event.target === slowButton) {
+        console.log("Left " + slowText + " button");
+      } else if (event.target === goButton) {
+        console.log("Left " + goText + " button");
+      }
+    })
+    ;
 
 
   });
